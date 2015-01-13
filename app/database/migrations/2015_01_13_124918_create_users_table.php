e4974cdaec48e7b4b682a44798eae6d6cb5e64b5
test <tigran.iguan@gmail.com>
2015-01-13 18:08:52 +0400

    add code

---
+++ app/database/migrations/2015_01_13_124918_create_users_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateUsersTable extends Migration {
+
+	/**
+	 * Run the migrations.
+	 *
+	 * @return void
+	 */
+	public function up()
+	{
+        Schema::create('users', function(Blueprint $table) {
+            $table->increments('id');
+            $table->string('username', 45);
+            $table->string('lastname', 45);
+            $table->string('email', 150);
+            $table->string('login', 45);
+            $table->strgin('password', 50);
+        });
+	}
+
+	/**
+	 * Reverse the migrations.
+	 *
+	 * @return void
+	 */
+	public function down()
+	{
+		//
+	}
+
+}
